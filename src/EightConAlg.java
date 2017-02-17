6e4f9175ebf05c258ab8ff90cf303adc5a37137d
radeshshiwlochan <radesh0430@gmail.com>
2017-02-17 16:38:34 -0500

    reads in the input file

--- src/EightConAlg.java
+++ src/EightConAlg.java
@@ -13,16 +13,51 @@ public class EightConAlg {
 	private int NeighborAry[];
 	private int EQAry[];
 	
-	public EightConAlg(String inputFile, String outputFile1,
-			              String outputFile2, String outputFile3) {
+	public EightConAlg(String inputFile) {
 		try {
-		Scanner readInput = new Scanner(new File(inputFile));
-		numRows = readInput.nextInt();
-		numCols = readInput.nextInt();
-		minVal = readInput.nextInt();
-		maxVal = readInput.nextInt();
-		} catch(IOException exc) {
+			Scanner readFile = new Scanner(new File(inputFile));
+			numRows = readFile.nextInt();
+			numCols = readFile.nextInt();
+			minVal = readFile.nextInt();
+			maxVal = readFile.nextInt();
+		
+			newLabel = 0;
+			int rowSize = numRows + 2, colSize = numCols + 2,
+					      EQArySize = (numRows * numCols)/2;
+			zeroFramedAry = new int[rowSize][colSize];
+			NeighborAry = new int[5];
+			EQAry = new int[EQArySize];
+			for(int rowIndex = 0; rowIndex < EQArySize; ++rowIndex )
+				EQAry[rowIndex] = rowIndex;
+			readFile.close();
+		} catch(Exception exc) {
 			System.out.println("Error Reading in the File " + exc);
+		}
+	}
+	
+	public void zeroFramed() {
+		//framing left to right
+		for(int i = 0; i <= numRows + 3; i++) {
+			zeroFramedAry[i][0]           = zeroFramedAry[i][2];
+			zeroFramedAry[i][1]           = zeroFramedAry[i][2];
+			zeroFramedAry[i][numCols + 2] = zeroFramedAry[i][numCols + 1];
+			zeroFramedAry[i][numCols + 3] = zeroFramedAry[i][numCols + 1];
+		}
+		//framing top to bottom
+		for(int j = 0; j <= numCols + 3; j++) {
+			 zeroFramedAry[0][j]           = zeroFramedAry[2][j];
+			 zeroFramedAry[1][j]           = zeroFramedAry[2][j];
+			 zeroFramedAry[numRows + 2][j] = zeroFramedAry[numRows + 1][j];
+			 zeroFramedAry[numRows + 3][j] = zeroFramedAry[numRows + 1 ][j];
+		}
+	}
+	
+	public void printFunc() {
+		for (int i = 0; i < numRows + 2; ++i) {
+			for(int j = 0; j < numCols + 2; ++j){
+				System.out.print(zeroFramedAry[i][j] + " ");
+			}
+			System.out.println();
 		}
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 
 public class Main {
 	public static void main(String [] args) {
-		if(args.length != 4) {
+		if(args.length != 1) {
 			System.out.println("Program needs 4 files as argument,"
 					+ " Terminating!!");
 			System.exit(-1);
 		}
+		try {
+		String inputFile   = args[0];
+			   //outputFile1 = args[1],
+			   //outputFile2 = args[2],
+			   //outputFile3 = args[3];
+		EightConAlg eightConAlg = new EightConAlg(inputFile);
+		eightConAlg.printFunc();
+			
 		
-		String inputFile   = args[0],
-			   outputFile1 = args[1],
-			   outputFile2 = args[2],
-			   outputFile3 = args[3];
-		EightConAlg eightConAlg = new EightConAlg(inputFile, outputFile1,
-				                       outputFile2, outputFile3);
-				                            
+		} catch(Exception exc) {
+			System.out.println(exc);
+		}
 	}
 }
