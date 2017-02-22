d5fd588b31c8376027ab9394f5d8d4bfa8e4249b
radeshshiwlochan <radesh0430@gmail.com>
2017-02-21 23:34:37 -0500

    prints output to file

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 
 public class Main {
 	public static void main(String [] args) {
-		if(args.length != 1) {
+		if(args.length != 2) {
 			System.out.println("Program needs 4 files as argument,"
 					+ " Terminating!!");
 			System.exit(-1);
 		}
 		try {
-		String inputFile   = args[0];
-			   //outputFile1 = args[1],
+		String inputFile   = args[0],
+			   outputFile1 = args[1];
 			   //outputFile2 = args[2],
 			   //outputFile3 = args[3];
 		EightConAlg eightConAlg = new EightConAlg(inputFile);
 		eightConAlg.zeroFramed();
 		eightConAlg.loadImage(inputFile);
 		eightConAlg.EightConCC_Pass1();
-		//eightConAlg.printFunc();
+		eightConAlg.printFunc(outputFile1);
 			
 		
 		} catch(Exception exc) {
